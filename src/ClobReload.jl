4a6426a630140f455360a3c15a3fa3cc6ce1f370
Cedric St-Jean <cedric.stjean@gmail.com>
2016-09-19 20:09:18 -0400

    First commit - most of the code is there.

--- src/ClobReload.jl
+++ src/ClobReload.jl
@@ -1,5 +1,79 @@
 module ClobReload
 
-# package code goes here
+using MacroTools
+
+export creload
+
+""" `parse_module_file(fname)` parses the file named `fname` as a module file
+(i.e. `module X ... end`) and returns `(module_name, code::Vector)`. """
+function parse_module_file(fname::String)
+    try
+        mod_expr = open(fname) do f
+            s = readstring(f)
+            parse(s)
+        end
+
+        @assert(@capture mod_expr module modname_ code__ end)
+        return modname, code
+    catch e
+        error("Cannot parse $fname; must contain a single module. Exception $e")
+    end
+end
+
+""" `parse_included_file(filename)` returns the expressions in `filename` as a
+`Vector` of expressions """
+function parse_included_file(filename)
+    # From Autoreload.jl
+    source = string("begin\n", open(filename) do f; readstring(f) end, "\n end")
+    parsed = parse(source)
+    @assert(@capture parsed begin code__ end)
+    return code
+end
+
+gather_includes(code::Vector) = 
+    mapreduce(expr->(@match expr begin
+        include(fname_) => [fname]
+        any_ => []
+    end), vcat, code)
+
+"""    gather_all_module_files(mod_name::String)
+
+Given a module name (as a string), returns the list of all files that define
+this module (i.e. the module name + all included files, applied recursively)
+"""
+function gather_all_module_files(mod_name::String)
+    mod_path = Base.find_in_node_path("AA", nothing, 1)
+    included_files = Set{String}([mod_path]) # to be filled
+    gather(full_path, parse_fun) = 
+        cd(dirname(full_path)) do
+            mod_includes = map(abspath,
+                               gather_includes(parse_fun(basename(full_path))))
+        end
+    function rec(path::String)
+        if !(path in included_files)
+            push!(included_files, path)
+            map(rec, gather(path, parse_included_file))
+        end
+    end
+    map(rec, gather(mod_path, mod->parse_module_file(mod)[2]))
+    return included_files
+end
+
+
+""" `creload(mod_name)` reloads `mod_name` by executing the module code inside
+the **existing** module. So unlike `reload`, `creload` does not create a new
+module objects; it merely clobbers the existing definitions therein. """
+function creload(mod_name)
+    info("Reloading $mod_name")
+    mod_path = Base.find_in_node_path(mod_name, nothing, 1)
+    cd(dirname(mod_path)) do
+        real_mod_name, code = parse_module_file(basename(mod_path))
+        eval(eval(real_mod_name), :(begin $(code...) end));
+        # For areload()
+        
+        module_was_loaded!(mod_name)
+    end
+end
+
 
 end # module
---
+++ src/autoreload.jl
@@ -0,0 +1,73 @@
+export areload, @ausing, @aimport
+
+
+hook_registered = false
+function register_hook!() # for IJulia
+    if !hook_registered
+        global hook_registered = true
+        IJulia.push_preexecute_hook(autoreload)
+    end
+end
+
+# Filename => Set(Modules_That_Include_Filename)
+file2mods = Dict{String, Set{String}}() # TODO: const
+# Module => Last_Time_It_Was_Loaded
+module2time = Dict{String, Number}()
+
+function module_was_loaded!(mod_name)
+    module2time[mod_name] = time()
+    register_module_files!(mod_name)
+end
+        
+
+"""    register_module_files!(mod_name::String)
+
+Remembers all the files included by `mod_name` """
+function register_module_files!(mod_name::String)
+    for fname in gather_all_module_files(mod_name)
+        push!(get!(file2mods, fname, Set{String}()), mod_name)
+    end
+end
+
+function modified_modules()
+    modified_mods = Set{String}()
+    for (fname, module_names) in file2mods
+        for mod in module_names
+                # Was it modified after the last time the module was loaded?
+            if mtime(fname) > module2time[mod]
+                push!(modified_mods, mod)
+            end
+        end
+    end
+    return modified_mods
+end
+
+
+macro ausing(mod_sym::Symbol)
+    # call using
+    # register the module file for reloading
+    esc(:(begin
+        using $mod_sym
+        module_was_loaded!($(string(mod_sym)))
+        register_hook!()
+    end))
+end
+
+macro aimport(mod_sym::Symbol)
+    # call using
+    # register the module file for reloading
+    esc(:(begin
+        import $mod_sym
+        module_was_loaded!($(string(mod_sym)))
+        register_hook!()
+    end))
+end
+
+
+""" `areload()` reloads (using `creload`) all modules that have been modified
+since they were last loaded. """
+function areload()
+    for mod_name in modified_modules()
+        creload(mod_name)
+    end
+end
