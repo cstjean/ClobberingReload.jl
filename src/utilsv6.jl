209466ef6d6a61762013c358438756f0642ea5fc
Cedric St-Jean <cedric.stjean@gmail.com>
2017-07-12 22:20:13 -0400

    Handle nested where parameters in longdef1

--- src/utilsv6.jl
+++ src/utilsv6.jl
@@ -1,15 +1,29 @@
 # Code that doesn't parse under Julia 0.5
 # This is part of fundef.jl
 
+function gather_wheres(ex)
+    if @capture(ex, (f_ where {params1__}))
+        f2, params2 = gather_wheres(f)
+        (f2, (params1..., params2...))
+    else
+        (ex, ())
+    end
+end
+
 function longdef1_where(ex)
-    @match ex begin
-        (f_(args__) where {whereparams__} = body_) =>
-            @q function $f($(args...)) where {$(whereparams...)}
-                $body end
-        ((f_(args__)::rtype_) where {whereparams__} = body_) =>
-            @q function ($f($(args...))::$rtype) where {$(whereparams...)}
-                $body end
-        _ => ex
+    if @capture(ex, (fcall_ = body_))
+        fcall2, whereparams = gather_wheres(fcall)
+        @match fcall2 begin
+            (f_(args__)) =>
+                @q function $f($(args...)) where {$(whereparams...)}
+                    $body end
+            (f_(args__)::rtype_) =>
+                @q function ($f($(args...))::$rtype) where {$(whereparams...)}
+                    $body end
+            _ => ex
+        end
+    else
+        ex
     end
 end
 function splitwhere(fdef)
